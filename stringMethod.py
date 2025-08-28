59dc5469b6c0ca0184c0ad644ab86bfcb1621a41
Darkadin14 <darkdesigner92@gmail.com>
2025-08-28 22:46:15 +0600

    first commit

---
+++ stringMethod.py
@@ -0,0 +1,21 @@
+name = "Darkadin"
+
+#print(len(name))
+
+#print(name.find("a"))
+
+#print(name.capitalize())
+
+#print(name.upper())
+
+#print(name.lower())
+
+#print(name.isdigit())
+
+#print(name.isalpha())
+
+#print(name.count("a"))
+
+#print(name.replace("a", "o"))
+
+print((name + " ") * 100)
---
+++ variables.py
@@ -0,0 +1,7 @@
+first_name = "Bro"
+last_name = "Code"
+print("Your name is: " + (first_name))
+
+age, gender = 15, "male"
+person = age, gender
+print(person)
