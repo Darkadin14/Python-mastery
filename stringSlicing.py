8c642bb54435f93ee92285d74a82a290b49118f6
Darkadin14 <darkdesigner92@gmail.com>
2025-08-29 22:16:39 +0600

    3rd day

---
+++ stringSlicing.py
@@ -0,0 +1,34 @@
+# basically creating substrings from strings
+#Methods: slice() or indexing[]
+#[start:stop:step]
+
+
+#name = "Fardin Ibne Quddus"
+
+#first_name = name[0:6]
+#mid_name = name[7:11]
+#last_name = name[12:]
+
+#print(first_name)
+#print(mid_name)
+#print(last_name)
+
+#first_name1 = name[0:6:7]
+#mid_name1 = name[7:11:5]
+#last_name1 = name[12: :7]
+
+#print(first_name1 
+#      +" "+
+#      mid_name1
+#      +" "+
+#      last_name1)
+
+
+
+website = "https://google.com"
+website1 = "https://wikipedia.com"
+
+slice = slice(8, -4)
+
+print(website[slice])
+print(website1[slice])
--- userInput.py
+++ userInput.py
@@ -1,14 +1,9 @@
 name = str(input("What is your name? :"))
 age = int(input("What is your age? :"))
 u = (input("How tall are you in cm? :"))
-for i in range(1):
-  if u == int:
-    u = int(u)
-  elif u == float:
-    u = float(u)
-  if u == float or int:
-    print("Your name is " + name)
-    print("Your age is " + str(age) + "years old")
-    print("Your height is " + str(u) + "cm")
-  else:
-    break
+print("Your name is " + name)
+if u == float or int:
+  print("Your age is " + str(age) + "years old")
+  print("Your height is " + str(u) + "cm")
+else:
+  print("TYPE INT OR DECIMALS!")
