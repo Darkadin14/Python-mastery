dd9567fc4f5ad9e0c61a87892af208f7f61b6d31
Darkadin14 <darkdesigner92@gmail.com>
2025-09-04 22:38:26 +0600

    7th day (not much)

--- dictionary.py
+++ dictionary.py
@@ -1,3 +1,28 @@
 # dictionary = collection which is UNORDERED, CHANGEABLE of unique key : value pairs
 #Fast because they use hashing, allow us to access a value quickly
 
+capital = {"USA":"Washington DC",
+           "India":"New Delhi",
+           "China":"Beijing",
+           "Russia":"Moscow"}
+
+#print(capital["USA"])
+
+#print(capital.get("Nazi"))
+
+#print(capital.keys())
+
+#print(capital.values())
+
+#print(capital.items())
+
+capital.update({"Bangladesh":"Dhaka"})
+
+capital.update({"USA":"Sylhet"})
+
+capital.pop("USA")
+
+#capital.clear()
+
+for key, values in capital.items():
+  print(key,"-", values)
