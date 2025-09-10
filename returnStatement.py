619eb5ef752bd13986fbd2ca6a74ce8e1fcb9ed4
Darkadin14 <darkdesigner92@gmail.com>
2025-09-10 22:53:22 +0600

    nothing....

---
+++ returnStatement.py
@@ -0,0 +1,14 @@
+# return statements = Functions send Python values/objects back to the caller.
+#                     These values/objects are known as function's return value
+
+#def multiply(num1, num2):
+  #result = num1 * num2
+  #return result
+
+#OR
+
+def multiply(num1, num2): 
+  return num1 * num2
+
+
+print(multiply(int(input("num1 = ")), int(input("num2 = "))))
